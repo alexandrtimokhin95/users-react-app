5c0746c6ea3466756c9aeb30a921a4c7c2a88c06
Alexandr <alexandr.timikhin95@mail.ru>
2022-08-30 23:15:40 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import { Users } from './components/Users';
 
 function App() {
   const [users, setUsers] = React.useState([]);
-  // const [invites, setInvites] = React.useState([]);
+  const [invites, setInvites] = React.useState([]);
   const [isLoading, setLoading] = React.useState(true);
+  const [success, setSuccess] = React.useState(false);
   const [searchValue, setSearchValue] = React.useState('');
 
   React.useEffect(() => {
@@ -24,13 +25,34 @@ function App() {
     setSearchValue(event.target.value);
   }
 
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites((prev) => prev.filter((_id) => _id !== id));
+    } else {
+      setInvites((prev) => [...prev, id]);
+    }
+  }
+
+  const onClickSendInvites = () => {
+    setSuccess(true);
+  }
+
   return (
     <div className="App">
-      <Users
-        onChangeSearchValue={onChangeSearchValue}
-        searchValue={searchValue}
-        items={users}
-        isLoading={isLoading}/>
+      {success ? (
+         <Success count={invites.length} />
+      ) : (
+        <Users
+          onChangeSearchValue={onChangeSearchValue}
+          searchValue={searchValue}
+          items={users}
+          isLoading={isLoading}
+          invites={invites}
+          onClickInvite={onClickInvite}
+          onClickSendInvites={onClickSendInvites}
+        />
+      )}
+
       {/* <Success /> */}
     </div>
   );
--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const User = ({ id, email, first_name, last_name, avatar }) => (
+export const User = ({ id, email, first_name, last_name, avatar, onClickInvite, isInvited }) => (
   <li>
     <div>
       <img className="avatar" src={avatar} alt="User" />
@@ -14,6 +14,23 @@ export const User = ({ id, email, first_name, last_name, avatar }) => (
         </p>
       </div>
     </div>
-    <img className="action" src="/assets/minus.svg" alt="Action" />
+    <div className="action-svg" onClick={() => onClickInvite(id)}>
+      {isInvited ? (
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16">
+          <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
+        </svg>
+      ) : (
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+          <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+        </svg>
+      )}
+    </div>
+
+    {/* <img 
+      onClick={() => onClickInvite(id)} 
+      className="action" 
+      src={`/assets/images/${isInvited ? 'minus' : 'plus'}.svg`} 
+      alt="Action" 
+    /> */}
   </li>
 );
