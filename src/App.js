913cb703d119546593e73b17848fa474e7cd69ad
Alexandr <alexandr.timikhin95@mail.ru>
2022-08-30 20:35:07 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,34 @@ import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
 
-// Тут список пользователей: https://reqres.in/api/users
-
 function App() {
+  const [users, setUsers] = React.useState([]);
+  // const [invites, setInvites] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(true);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  React.useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then(res => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      }).catch((err) => {
+        console.warn(err);
+        alert('Ошибка при получении пользователя');
+      }).finally(() => setLoading(false));
+  }, []);
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  }
+
   return (
     <div className="App">
-      <Users />
+      <Users
+        onChangeSearchValue={onChangeSearchValue}
+        searchValue={searchValue}
+        items={users}
+        isLoading={isLoading}/>
       {/* <Success /> */}
     </div>
   );
